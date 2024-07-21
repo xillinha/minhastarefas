80c41d7e3d96e782c66e6de2215edab154e47c5f
Priscilla Ramsay <xillinha@gmail.com>
2024-07-21 14:45:48 -0400

    Criação do projeto

---
+++ src/main/java/com/xillinha/MinhastarefasApplication.java
@@ -0,0 +1,13 @@
+package com.xillinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhastarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhastarefasApplication.class, args);
+	}
+
+}
